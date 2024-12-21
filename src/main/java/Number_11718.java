bd6ea794cb8e382baaba7bc61ef10b81189f58d3
Zrfv <youngoh07015@gmail.com>
2024-12-22 00:35:25 +0900

    문제 11718 풀이

---
+++ src/main/java/Number_11718.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number_11718 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        while ((input = bufferedReader.readLine()) != null) {
+            System.out.println(input);
+        }
+    }
+}
