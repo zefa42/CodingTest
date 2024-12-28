9d3e1c1524812ee2ebd9518101ef1d94eb294494
Zrfv <youngoh07015@gmail.com>
2024-12-28 21:13:39 +0900

    정렬 알고리즘 수정

--- src/main/java/Number_1181.java
+++ src/main/java/Number_1181.java
@@ -2,8 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedHashSet;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -11,21 +10,20 @@ public class Number_1181 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(bufferedReader.readLine());
-        Set<String> words = new LinkedHashSet<>();
-        for (int i = 0; i < count; ++i) {
+        Set<String> words = new HashSet<>();
+        for(int i = 0; i < count; ++i) {
             words.add(bufferedReader.readLine());
         }
-        List<String> uniqueWords = new ArrayList<>(words);
 
-        // 정렬 (길이 → 사전순)
-        Collections.sort(uniqueWords, (word1, word2) -> {
+        List<String> result = new ArrayList<>(words);
+        result.sort((word1, word2) -> {
             if (word1.length() == word2.length()) {
-                return word1.compareTo(word2); // 길이가 같으면 사전순
+                return word1.compareTo(word2);
             }
-            return Integer.compare(word1.length(), word2.length()); // 길이로 정렬
+            return Integer.compare(word1.length(), word2.length());
         });
 
-        for (String word : uniqueWords) {
+        for(String word : result) {
             System.out.println(word);
         }
     }
