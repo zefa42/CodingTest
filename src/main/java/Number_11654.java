0643b0f47ed1baaf4df33229acc114bbc5f2c9a9
Zrfv <youngoh07015@gmail.com>
2025-01-07 23:17:02 +0900

    문제 11654 코드 간소화

--- src/main/java/Number_11654.java
+++ src/main/java/Number_11654.java
@@ -1,12 +1,7 @@
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 
 public class Number_11654 {
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        String input = bufferedReader.readLine();
-        int ascii = input.charAt(0);
-        System.out.println(ascii);
+        System.out.println(System.in.read());
     }
 }
