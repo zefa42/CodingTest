73f67c923ecb07b37d144605d7745c96384210ef
Zrfv <youngoh07015@gmail.com>
2025-01-10 23:34:17 +0900

    문제 16139 시간 복잡도 개선
    알파벳 별로 등장 횟수를 누적하여 배열에 저장하는 방식으로 변경(누적합)

--- src/main/java/Number_16139.java
+++ src/main/java/Number_16139.java
@@ -1,30 +1,37 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Number_16139 {
+    static final int ALPHABET_SIZE = 26;
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         String string = bufferedReader.readLine();
-        Map<Integer, String> index = new HashMap<>();
-        for(int i = 0; i < string.length(); ++i) {
-            index.put(i, String.valueOf(string.charAt(i)));
+
+        int stringSize = string.length();
+        int[][] prefix = new int[ALPHABET_SIZE][stringSize + 1];
+
+        for(int i = 0; i < stringSize; ++i) {
+            char alphabet = string.charAt(i);
+            int alphabetIndex = alphabet - 'a';
+
+            for(int c = 0; c < ALPHABET_SIZE; ++c) {
+                prefix[c][i + 1] = prefix[c][i];
+            }
+            prefix[alphabetIndex][i + 1]++;
         }
+
         int count = Integer.parseInt(bufferedReader.readLine());
         StringBuilder result = new StringBuilder();
+
         for(int i = 0; i < count; ++i) {
-            int sum = 0;
             String[] input = bufferedReader.readLine().split(" ");
-            String first = String.valueOf(input[0]);
+            char inputAlphabet = input[0].charAt(0);
             int start = Integer.parseInt(input[1]);
             int end = Integer.parseInt(input[2]);
-            for(int j = start; j <= end; ++j) {
-                if(index.get(j).equals(first)) {
-                    sum++;
-                }
-            }
+
+            int inputAlphabetIndex = inputAlphabet - 'a';
+            int sum = prefix[inputAlphabetIndex][end + 1] - prefix[inputAlphabetIndex][start];
             result.append(sum).append('\n');
         }
         System.out.println(result);
