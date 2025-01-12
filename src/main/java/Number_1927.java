6d9d4fe581d32397cebb4fc2701e454311a111d4
Zrfv <youngoh07015@gmail.com>
2025-01-12 21:08:39 +0900

    문제 1927 풀이
    중복된 숫자도 허용하기 위해 기존 TreeSet에서 PriorityQueue로 변경

---
+++ src/main/java/Number_1927.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Number_1927 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int count = Integer.parseInt(bufferedReader.readLine());
+
+        StringBuilder result = new StringBuilder();
+        PriorityQueue<Integer> numbers = new PriorityQueue<>();
+        for(int i = 0; i < count; ++i) {
+            int input = Integer.parseInt(bufferedReader.readLine());
+            if(input == 0) {
+                if (!numbers.isEmpty()) {
+                    result.append(numbers.poll()).append('\n');
+                }
+                else {
+                    result.append("0").append('\n');
+                }
+            }
+            else {
+                numbers.add(input);
+            }
+        }
+        System.out.println(result);
+    }
+}
