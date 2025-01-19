89e43cb2d4327b010e09fb7d57cefa9b8daef88f
Zrfv <youngoh07015@gmail.com>
2025-01-19 23:40:40 +0900

    refactor: 문제 11286 시간 복잡도 개선
    기존 마이너스 값들을 List로 관리하여 절대값을 비교하는 방식에서 PriorityQueue 자체에서 절대값도 정렬되도록 수정
    (1968ms -> 280ms)

--- src/main/java/Number_11286.java
+++ src/main/java/Number_11286.java
@@ -1,43 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.PriorityQueue;
 
 public class Number_11286 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(bufferedReader.readLine());
-        PriorityQueue<Long> queue = new PriorityQueue<>();
+        
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> {
+            if(Math.abs(a) == Math.abs(b)) {
+                return a - b;
+            }
+            return Math.abs(a) - Math.abs(b);
+        });
 
         StringBuilder result = new StringBuilder();
-        List<Long> minusNumbers = new ArrayList<>();
         for(int i = 0; i < count; ++i) {
-            long input = Long.parseLong(bufferedReader.readLine());
+            int input = Integer.parseInt(bufferedReader.readLine());
             if(input == 0) {
                 if(queue.isEmpty()) {
                     result.append(0).append('\n');
                 }
                 else {
-                    long peek = -queue.peek();
-                    if(minusNumbers.contains(peek)) {
-                        result.append(-(queue.poll())).append('\n');
-                        minusNumbers.remove(peek);
-                    }
-                    else {
-                        result.append(queue.poll()).append('\n');
-                    }
+                    result.append(queue.poll()).append('\n');
                 }
             }
             else {
-                if (input < 0) {
-                    queue.add(-input);
-                    minusNumbers.add(input);
-                }
-                else {
-                    queue.add(input);
-                }
+                queue.offer(input);
             }
         }
         System.out.println(result);
