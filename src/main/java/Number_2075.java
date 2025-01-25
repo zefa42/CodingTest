b9b357293b31bfc6a0b073f92570ffb3555c0ec2
zefa05 <youngoh07015@gmail.com>
2025-01-25 23:01:04 +0900

    refactor: 문제 2075 시간 복잡도 개선
    기존 ArrayList를 이용하는 방식에서 우선순위 큐로 변경
    (1836ms -> 896ms)

--- src/main/java/Number_2075.java
+++ src/main/java/Number_2075.java
@@ -1,28 +1,26 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 
 public class Number_2075 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        long startTime = System.currentTimeMillis();
         int count = Integer.parseInt(bufferedReader.readLine());
-        List<Integer> n = new ArrayList<>();
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
         for(int i = 0; i < count; ++i) {
             int[] numbers = Arrays.stream(bufferedReader.readLine().split(" "))
                     .mapToInt(Integer::parseInt)
                     .toArray();
-            for(int j = 0; j < count; ++j) {
-                n.add(numbers[j]);
+            for (int j = 0; j < count; ++j) {
+                queue.add(numbers[j]);
             }
         }
-        n.sort(Collections.reverseOrder());
-        System.out.println(n.get(count - 1));
-        long endTime = System.currentTimeMillis();
-        System.out.println(endTime - startTime);
+        for(int i = 0; i < count - 1; ++i) {
+            queue.poll();
+        }
+        System.out.println(queue.poll());
     }
 }
