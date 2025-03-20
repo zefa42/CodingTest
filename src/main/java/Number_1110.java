ef3ca1038344691d731be7469c41b683b617c8ab
zefa42 <youngoh07015@gmail.com>
2025-03-20 10:30:40 +0900

    문제 1110 풀이

---
+++ src/main/java/Number_1110.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number_1110 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int number = Integer.parseInt(bufferedReader.readLine());
+        int tmp = number;
+        int cycleCount = 0;
+        while(true) {
+            cycleCount++;
+            tmp = findNewNumber(tmp);
+            if(number == tmp) break;
+        }
+        System.out.println(cycleCount);
+    }
+
+    public static int findNewNumber(int number) {
+        int first = number % 10;
+        int tmp = number / 10;
+        int second = first + tmp;
+        int newNumber = (first * 10) + second;
+        if(newNumber > 9) {
+            return (first * 10) + (second % 10);
+        }
+        return newNumber;
+    }
+}
