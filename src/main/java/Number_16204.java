63814fb1971d2aca3f5a6283d9cae73dd70dc808
zefa05 <youngoh07015@gmail.com>
2025-03-23 19:56:15 +0900

    문제 16204 풀이

---
+++ src/main/java/Number_16204.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Number_16204 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int[] input = Arrays.stream(bufferedReader.readLine().split(" "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+        int result = Math.min(input[1], input[2]) + Math.min(input[0] - input[1], input[0] - input[2]);
+        System.out.println(result);
+    }
+}
