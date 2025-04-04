ff0585ac833ef6ea9b04a1801834581d8c993d00
zefa42 <youngoh07015@gmail.com>
2025-04-04 15:37:34 +0900

    refactor: 문제 15688 시간 복잡도 개선
    - ArrayList의 오토박싱/언박싱 오버헤드 발생으로 인해 원시형 int 배열로 변환하여 정렬하는 방식으로 시간 복잡도 개선

--- src/main/java/Number_15688.java
+++ src/main/java/Number_15688.java
@@ -1,23 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.Arrays;
 
 public class Number_15688 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         int count = Integer.parseInt(bufferedReader.readLine());
         StringBuilder result = new StringBuilder();
-        List<Integer> numbers = new ArrayList<>();
+        int[] numbers = new int[count];
         for(int i = 0; i < count; ++i) {
             int number = Integer.parseInt(bufferedReader.readLine());
-            numbers.add(number);
+            numbers[i] = (number);
         }
-        
-        Collections.sort(numbers);
-        for(Integer num : numbers) {
+
+        Arrays.sort(numbers);
+        for(int num : numbers) {
             result.append(num).append('\n');
         }
         System.out.println(result);
