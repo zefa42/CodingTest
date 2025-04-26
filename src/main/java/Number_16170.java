b42106a30ace58bc4d7684e05900c9704559f22a
zefa05 <youngoh07015@gmail.com>
2025-04-26 21:31:52 +0900

    문제 16170 풀이

---
+++ src/main/java/Number_16170.java
@@ -0,0 +1,7 @@
+public class Number_16170 {
+    public static void main(String[] args) {
+        System.out.println(2018);
+        System.out.println("09");
+        System.out.println(29);
+    }
+}
