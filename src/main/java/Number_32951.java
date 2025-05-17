24eb3afcbef4790668fcf7eabaa4525edb86df4c
zefa05 <youngoh07015@gmail.com>
2025-05-17 21:31:29 +0900

    문제 32951 풀이

---
+++ src/main/java/Number_32951.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Number_32951 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        int input = Integer.parseInt(bufferedReader.readLine());
+        System.out.println(input - 2024);
+    }
+}
