0a528933a3648c9fadcde76d67a9eb0d37cb1ab0
zefa42 <youngoh07015@gmail.com>
2025-06-13 11:19:29 +0900

    문제 10282 주석 추가

--- src/main/java/BaekJoon/Number_10282.java
+++ src/main/java/BaekJoon/Number_10282.java
@@ -8,6 +8,23 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.PriorityQueue;
 
+// 시작점에서 단방향으로 모든 노드를 탐색한다. (감염자 → 의존자로만 감염이 퍼짐)
+// 마지막 노드까지 탐색했을 때 감염된 모든 컴퓨터를 구하는 문제
+// 각 컴퓨터의 최단 감염 시간 dist[i] 를 구한다.
+//
+
+// dist[i] 가 INF 가 아닌 노드의 개수
+// → 감염된 컴퓨터 수
+//
+// 그 노드들 가운데 dist[i] 의 최댓값
+// → 마지막 컴퓨터가 감염되기까지 걸린 시간
+
+// 3 3 1 (컴퓨터 3개, 의존 개수, 시작 컴퓨터 번호)
+// 2 1 2 (2가 1을 의존 1 -(2초)> 2)
+// 3 1 8
+// 3 2 4
+
+
 public class Number_10282 {
     private static final int INF = Integer.MAX_VALUE;
 
@@ -40,6 +57,7 @@ public class Number_10282 {
                 int cost = inputDependency[2];
 
                 // b가 감염되면 s초 뒤 a 감염
+                // 한 지점에서 다른 지점에 대한 의존도를 저장하기 때문에 단방향으로 저장
                 graph.get(from).add(new Edge(to, cost));
             }
 
@@ -83,6 +101,9 @@ public class Number_10282 {
     }
 
     // 1개 간선 표현 (다음 정점, 가중치)
+    // 1: (2,2) (3,8)
+    // 2: (3,4)
+    // 3: —
     private static class Edge {
         int to;
         int cost;
@@ -103,6 +124,7 @@ public class Number_10282 {
             this.dist = dist;
         }
 
+        // compareTo() 메서드는 Node 객체끼리 “어느 것이 더 앞(작은 값)인가”를 결정
         @Override
         public int compareTo(Node o) {
             return Integer.compare(this.dist, o.dist);
