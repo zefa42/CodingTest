bee9fa4ef4dd2e9c88b8487fc59c1d9a35e2e854
zefa42 <youngoh07015@gmail.com>
2025-07-17 13:06:07 +0900

    문제 15964 수정

--- src/main/java/BaekJoon/Number_15964.java
+++ src/main/java/BaekJoon/Number_15964.java
@@ -1,3 +1,5 @@
+package BaekJoon;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,13 +8,13 @@ import java.util.Arrays;
 public class Number_15964 {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int[] input = Arrays.stream(bufferedReader.readLine().split(" "))
-                .mapToInt(Integer::parseInt)
+        long[] input = Arrays.stream(bufferedReader.readLine().split(" "))
+                .mapToLong(Long::parseLong)
                 .toArray();
 
-        int first = input[0] + input[1];
-        int second = input[0] - input[1];
-        int result = first * second;
+        long first = input[0] + input[1];
+        long second = input[0] - input[1];
+        long result = first * second;
         System.out.println(result);
     }
 }
