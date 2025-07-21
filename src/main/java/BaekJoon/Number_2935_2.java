1cf5904554babacce33b2ac7b344d492344d416a
zefa42 <youngoh07015@gmail.com>
2025-07-21 17:29:31 +0900

    문제 2935 풀이

---
+++ src/main/java/BaekJoon/Number_2935_2.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Number_2935_2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BigInteger first = new BigInteger(bufferedReader.readLine());
+        String sign = bufferedReader.readLine();
+        BigInteger second = new BigInteger(bufferedReader.readLine());
+
+        if(sign.equals("+")) {
+            System.out.println(first.add(second));
+        }
+        else if(sign.equals("*")){
+            System.out.println(first.multiply(second));
+        }
+    }
+}
