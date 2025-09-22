533357c0eeb6ce5fe666733818927cc5790cdab3
Zefa <150803139+zefa42@users.noreply.github.com>
2025-09-22 23:58:52 +0900

    문제 기능개발 풀이

---
+++ src/main/java/Programmers/기능_개발.java
@@ -0,0 +1,40 @@
+package Programmers;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class 기능_개발 {
+    public static void main(String[] args) {
+        int[] result = solution(new int[] {93,30,55},  new int[] {1,30,5});
+        for(int val : result){
+            System.out.print(val + " ");
+        }
+    }
+    public int[] solution(int[] progresses, int[] speeds) {
+        int size = progresses.length;
+        int[] durations = new int[size];
+        Map<Integer, Integer> map = new LinkedHashMap<>();
+        for(int i = 0; i < size; ++i) {
+            int progress = progresses[i];
+            int speed = speeds[i];
+            int duration = 0;
+            while(true) {
+                progress += speed;
+                duration++;
+                if(progress >= 100) break;
+            }
+            durations[i] = duration;
+            if(i > 0 && durations[i] < durations[i-1]) {
+                durations[i] = durations[i-1];
+            }
+            map.put(durations[i], map.getOrDefault(durations[i], 0) + 1);
+        }
+        int[] result = new int[map.size()];
+        int i = 0;
+        for(int key : map.keySet()) {
+            result[i++] = map.get(key);
+        }
+        return result;
+    }
+}
